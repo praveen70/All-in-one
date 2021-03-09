114960fa3d6460c17dcc850ee35aadf90e4fd539
Praveen MR <praveen.mr@datafoundry.ai>
2021-03-09 16:58:19 +0530

    first

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken')
 
 // const path = require('path');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 connection.db.connect((err) => {
